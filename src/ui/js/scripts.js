56355bf9b734f464928f89fb3f9416c3dc8b5d66
Jonathan de Kock <jonathan@wapp.co.za>
2020-02-02 09:05:19 +0200

    Create UI - Main Menu | Add Menu shader for in-game menus

--- src/ui/js/scripts.js
+++ src/ui/js/scripts.js
@@ -2,10 +2,28 @@ var $ = require('jquery');
 
 $(document).ready(function () {
 	$(document).on('click', '#show-in-game-menu', function(){
+		$('.shader').show();
 		$('#in-game-menu').show();
 	});
 
 	$(document).on('click', '#hide-in-game-menu', function(){
+		$('.shader').hide();
+		$('#in-game-menu').hide();
+	});
+
+	$(document).on('click', '#start-game', function(){
+		$('body').addClass('loaded');
+		setTimeout(function () {
+			$('#loader-wrapper').hide();
+			$('#start-menu').hide();
+		}, 1500);
+	});
+
+	$(document).on('click', '#end-game', function(){
+		$('body').removeClass('loaded');
+		$('#loader-wrapper').show();
+		$('#start-menu').show();
+		$('.shader').hide();
 		$('#in-game-menu').hide();
 	});
 });
